8c6299f446925fe90cb9fe37b6c0267e2eeaf1c9
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-02-28 15:07:24 -0700

    built boards controller

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,50 @@
+class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :destroy, :update, :edit]
+
+  def index
+    @boards = Board.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @board.update(board_params)
+      redirect_to boards_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @board.destroy
+    redirect_to boards_path
+  end 
+
+  private
+  def set_board
+    @board = Board.find(params[:id])
+  end
+
+  def board_params
+    params.require(:board).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'boards#index'
+
+  resources :boards do
+    resources :lists
+  end
+
+  resources :lists do
+    resources :tasks
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
